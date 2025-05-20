af6adacaeafede4f6f8ac63753e43a1756ddc988
GuyGabriell <jonahgabriel85@yahoo.com>
2025-05-20 17:04:43 +0100

    second commit

---
+++ L3-start/src/App.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+import { ref } from 'vue'
+import socksgreenImage from './assets/images/socks_green.jpeg'
+
+const product = ref('Socks')
+const image = ref(socksgreenImage)
+const myUrl = ref('https://example.com')
+
+</script>
+
+//add a Url ref to your script section.
+//use v-bind to bind the url to the anchor tag href attribute.
+
+<template>
+  <div class="nar-bar"></div>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <!-- image goes here -->
+          <img v-bind:src="image"/>
+      </div>
+    
+      <div class="product-info">
+        <h1>{{ product }}</h1>
+      </div>
+        <div>
+        <a v-bind:href="myUrl"></a>
+      </div>
+    </div>
+  </div>
+</template>
