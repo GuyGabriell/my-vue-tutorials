81a57b524808c807bd463bd180e9704cb7634fb2
GuyGabriell <jonahgabriel85@yahoo.com>
2025-05-20 13:25:56 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+
+const product = ref('Jacket')
+</script>
+
+//add a product description ref to your script section.
+//displiay the description using an expression in a  p tag.
+
+<template>
+  <div class="product-display">
+    <div class="product-container">
+      <div class="product-info">
+        <p>{{product}}</p>
+      </div>
+    </div>
+  </div>
+</template>
